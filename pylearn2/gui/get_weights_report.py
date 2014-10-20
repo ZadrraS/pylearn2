71fe394f558fbce953d2f0b685501cc36cec4d09
ZadrraS <ZadrraS@gmail.com>
2014-10-20 23:38:51 +0300

    Pretrained layer weight visualization fix.

--- pylearn2/gui/get_weights_report.py
+++ pylearn2/gui/get_weights_report.py
@@ -3,19 +3,12 @@
 
     WRITEME
 """
-import logging
 from pylearn2.utils import serial
 from pylearn2.gui import patch_viewer
 from pylearn2.config import yaml_parse
 from pylearn2.datasets import control
+import pylearn2.models.mlp as mlp_models
 import numpy as np
-
-
-from pylearn2.utils.exc import reraise_as
-
-
-logger = logging.getLogger(__name__)
-
 
 def get_weights_report(model_path=None,
                        model=None,
@@ -31,15 +24,15 @@ def get_weights_report(model_path=None,
     model_path : str
         Filepath of the model to make the report on.
     rescale : str
-        A string specifying how to rescale the filter images:
-            - 'individual' (default) : scale each filter so that it
-                  uses as much as possible of the dynamic range
-                  of the display under the constraint that 0
-                  is gray and no value gets clipped
-            - 'global' : scale the whole ensemble of weights
-            - 'none' :   don't rescale
-    dataset : pylearn2.datasets.dataset.Dataset
-        Dataset object to do view conversion for displaying the weights. If
+        A string specifying how to rescale the filter images: \
+            'individual' (default): scale each filter so that it \
+                uses as much as possible of the dynamic range \
+                of the display under the constraint that 0 \
+                is gray and no value gets clipped \
+            'global' : scale the whole ensemble of weights \
+            'none' :   don't rescale
+    dataset: pylearn2.datasets.dataset.Dataset
+        Dataset object to do view conversion for displaying the weights. If \
         not provided one will be loaded from the model's dataset_yaml_src.
 
     Returns
@@ -48,10 +41,10 @@ def get_weights_report(model_path=None,
     """
 
     if model is None:
-        logger.info('making weights report')
-        logger.info('loading model')
+        print 'making weights report'
+        print 'loading model'
         model = serial.load(model_path)
-        logger.info('loading done')
+        print 'loading done'
     else:
         assert model_path is None
     assert model is not None
@@ -69,6 +62,8 @@ def get_weights_report(model_path=None,
         raise ValueError('rescale=' + rescale +
                          ", must be 'none', 'global', or 'individual'")
 
+    if isinstance(model.layers[0], mlp_models.PretrainedLayer):
+        model = model.layer_content
 
     if isinstance(model, dict):
         #assume this was a saved matlab dictionary
@@ -80,18 +75,18 @@ def get_weights_report(model_path=None,
         if len(keys) > 2:
             key = None
             while key not in keys:
-                logger.info('Which is the weights?')
+                print 'Which is the weights?'
                 for key in keys:
-                    logger.info('\t{0}'.format(key))
+                    print '\t', key
                 key = raw_input()
         else:
             key, = keys
         weights = model[key]
 
         norms = np.sqrt(np.square(weights).sum(axis=1))
-        logger.info('min norm: {0}'.format(norms.min()))
-        logger.info('mean norm: {0}'.format(norms.mean()))
-        logger.info('max norm: {0}'.format(norms.max()))
+        print 'min norm: ',norms.min()
+        print 'mean norm: ',norms.mean()
+        print 'max norm: ',norms.max()
 
         return patch_viewer.make_viewer(weights,
                                         is_color=weights.shape[1] % 3 == 0)
@@ -105,28 +100,36 @@ def get_weights_report(model_path=None,
     except NotImplementedError:
 
         if dataset is None:
-            logger.info('loading dataset...')
+            print 'loading dataset...'
             control.push_load_data(False)
             dataset = yaml_parse.load(model.dataset_yaml_src)
             control.pop_load_data()
-            logger.info('...done')
+            print '...done'
 
         try:
             W = model.get_weights()
         except AttributeError, e:
-            reraise_as(AttributeError("""
+            raise AttributeError("""
 Encountered an AttributeError while trying to call get_weights on a model.
 This probably means you need to implement get_weights for this model class,
 but look at the original exception to be sure.
 If this is an older model class, it may have weights stored as weightsShared,
 etc.
-Original exception: """+str(e)))
+Original exception: """+str(e))
 
     if W is None and weights_view is None:
         raise ValueError("model doesn't support any weights interfaces")
 
     if weights_view is None:
-        weights_format = model.get_weights_format()
+        if hasattr(model,'get_weights_format'):
+            weights_format = model.get_weights_format()
+        elif hasattr(model, 'weights_format'):
+            weights_format = model.weights_format
+        else:
+            # assume default
+            weights_format = ('v', 'h')
+
+
         assert hasattr(weights_format,'__iter__')
         assert len(weights_format) == 2
         assert weights_format[0] in ['v','h']
@@ -160,7 +163,7 @@ Original exception: """+str(e)))
         weights_view /= np.abs(weights_view).max()
 
     if norm_sort:
-        logger.info('sorting weights by decreasing norm')
+        print 'sorting weights by decreasing norm'
         idx = sorted( range(h), key=lambda l : - norm_prop[l] )
     else:
         idx = range(h)
@@ -174,18 +177,17 @@ Original exception: """+str(e)))
         patch = weights_view[idx[i],...]
         pv.add_patch(patch, rescale=patch_rescale, activation=act)
 
-    abs_weights = np.abs(weights_view)
-    logger.info('smallest enc weight magnitude: {0}'.format(abs_weights.min()))
-    logger.info('mean enc weight magnitude: {0}'.format(abs_weights.mean()))
-    logger.info('max enc weight magnitude: {0}'.format(abs_weights.max()))
+    print 'smallest enc weight magnitude: '+str(np.abs(weights_view).min())
+    print 'mean enc weight magnitude: '+str(np.abs(weights_view).mean())
+    print 'max enc weight magnitude: '+str(np.abs(weights_view).max())
 
 
     if W is not None:
         norms = np.sqrt(np.square(W).sum(axis=1))
         assert norms.shape == (h,)
-        logger.info('min norm: {0}'.format(norms.min()))
-        logger.info('mean norm: {0}'.format(norms.mean()))
-        logger.info('max norm: {0}'.format(norms.max()))
+        print 'min norm: ',norms.min()
+        print 'mean norm: ',norms.mean()
+        print 'max norm: ',norms.max()
 
     return pv
 
@@ -204,15 +206,14 @@ def get_binocular_greyscale_weights_report(model_path=None,
     model_path : str
         Filepath of the model to make the report on.
     rescale : str
-        A string specifying how to rescale the filter images:
-
-          - 'individual' (default) : scale each filter so that it
-            uses as much as possible of the dynamic range
-            of the display under the constraint that 0
-            is gray and no value gets clipped
-          - 'global' : scale the whole ensemble of weights
-          - 'none' : don't rescale
-    dataset : pylearn2.datasets.dataset.Dataset
+        A string specifying how to rescale the filter images: \
+            'individual' (default): scale each filter so that it \
+                uses as much as possible of the dynamic range \
+                of the display under the constraint that 0 \
+                is gray and no value gets clipped \
+            'global' : scale the whole ensemble of weights \
+            'none' :   don't rescale
+    dataset: pylearn2.datasets.dataset.Dataset
         Dataset object to do view conversion for displaying the weights. If \
         not provided one will be loaded from the model's dataset_yaml_src.
 
@@ -222,10 +223,10 @@ def get_binocular_greyscale_weights_report(model_path=None,
     """
 
     if model is None:
-        logger.info('making weights report')
-        logger.info('loading model')
+        print 'making weights report'
+        print 'loading model'
         model = serial.load(model_path)
-        logger.info('loading done')
+        print 'loading done'
     else:
         assert model_path is None
     assert model is not None
@@ -252,9 +253,9 @@ def get_binocular_greyscale_weights_report(model_path=None,
         weights ,= model.values()
 
         norms = np.sqrt(np.square(weights).sum(axis=1))
-        logger.info('min norm: {0}'.format(norms.min()))
-        logger.info('mean norm: {0}'.format(norms.mean()))
-        logger.info('max norm: {0}'.format(norms.max()))
+        print 'min norm: ',norms.min()
+        print 'mean norm: ',norms.mean()
+        print 'max norm: ',norms.max()
 
         return patch_viewer.make_viewer(weights,
                                         is_color=weights.shape[1] % 3 == 0)
@@ -268,22 +269,22 @@ def get_binocular_greyscale_weights_report(model_path=None,
     except NotImplementedError:
 
         if dataset is None:
-            logger.info('loading dataset...')
+            print 'loading dataset...'
             control.push_load_data(False)
             dataset = yaml_parse.load(model.dataset_yaml_src)
             control.pop_load_data()
-            logger.info('...done')
+            print '...done'
 
         try:
             W = model.get_weights()
         except AttributeError, e:
-            reraise_as(AttributeError("""
+            raise AttributeError("""
 Encountered an AttributeError while trying to call get_weights on a model.
 This probably means you need to implement get_weights for this model class,
 but look at the original exception to be sure.
 If this is an older model class, it may have weights stored as weightsShared,
 etc.
-Original exception: """+str(e)))
+Original exception: """+str(e))
 
     if W is None and weights_view is None:
         raise ValueError("model doesn't support any weights interfaces")
@@ -331,7 +332,7 @@ Original exception: """+str(e)))
         weights_view /= np.abs(weights_view).max()
 
     if norm_sort:
-        logger.info('sorting weights by decreasing norm')
+        print 'sorting weights by decreasing norm'
         idx = sorted(range(h), key=lambda l : - norm_prop[l])
     else:
         idx = range(h)
@@ -348,17 +349,16 @@ Original exception: """+str(e)))
         pv.add_patch(patch[:,:,1], rescale=False, activation=act)
         pv.add_patch(patch[:,:,0], rescale=False, activation=act)
 
-    abs_weights = np.abs(weights_view)
-    logger.info('smallest enc weight magnitude: {0}'.format(abs_weights.min()))
-    logger.info('mean enc weight magnitude: {0}'.format(abs_weights.mean()))
-    logger.info('max enc weight magnitude: {0}'.format(abs_weights.max()))
+    print 'smallest enc weight magnitude: '+str(np.abs(weights_view).min())
+    print 'mean enc weight magnitude: '+str(np.abs(weights_view).mean())
+    print 'max enc weight magnitude: '+str(np.abs(weights_view).max())
 
 
     if W is not None:
         norms = np.sqrt(np.square(W).sum(axis=1))
         assert norms.shape == (h,)
-        logger.info('min norm: {0}'.format(norms.min()))
-        logger.info('mean norm: {0}'.format(norms.mean()))
-        logger.info('max norm: {0}'.format(norms.max()))
+        print 'min norm: ',norms.min()
+        print 'mean norm: ',norms.mean()
+        print 'max norm: ',norms.max()
 
     return pv
